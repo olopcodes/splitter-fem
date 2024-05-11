e846a3792a5b3d5595143e574efa9bf79302c968
olopcodes <olopcodes@gmail.com>
2024-05-11 12:18:35 -0400

    finished app script, made changes for theme toggler

---
+++ js/app.js
@@ -0,0 +1,125 @@
+
+
+const splitter = {
+    billAmount: 0,
+    tipPercentage: 0,
+    numberOfPeople:0,
+    tipPerPerson:0,
+    totalPerPerson:0,
+
+    setTipPerPerson() {
+            let temp = Number((this.billAmount * this.tipPercentage) / this.numberOfPeople);
+            this.tipPerPerson = +temp.toFixed(2);
+    },
+
+    setTotalPerPerson() {
+        
+            let temp = (this.billAmount / this.numberOfPeople) + this.tipPerPerson;
+            this.totalPerPerson = +temp.toFixed(2);
+    },
+
+    checkValues() {
+        if(this.billAmount && this.tipPercentage && this.numberOfPeople) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+const tipPerPersonAmountEl = $('.tip-per-person-amount');
+const totalPerPersonAmountEl = $('.total-per-person-amount');
+
+$(document).ready(function () {
+    // start the app
+    app();
+    
+    // theme toggler
+    toggleTheme();
+});
+
+function checkForZeroes (value) {
+    // make an error message appear and disappear
+    // check color of input to invalid
+    if(value=== 0) {
+        alert(`value can't be zero`);
+        return true;
+    }
+};
+
+function getCalculations() {
+    // run this when all the values needed are stored
+    if(splitter.checkValues()) {
+        splitter.setTipPerPerson();
+        splitter.setTotalPerPerson();
+        tipPerPersonAmountEl.text(`$${splitter.tipPerPerson}`);
+        totalPerPersonAmountEl.text(`$${splitter.totalPerPerson}`);
+    }
+}
+
+function setupValues(id, value) {
+    if(!checkForZeroes(value)) {
+
+        if(id === 'bill-amount') {
+            splitter.billAmount = value;
+        }
+    
+        if(id === 'custom-tip-amount') {
+            splitter.tipPercentage = value / 100;
+        }
+    
+        if(id === 'number-of-people') {
+            splitter.numberOfPeople = value;
+        }
+
+    }
+};
+
+
+function resetValues () {
+    $(tipPerPersonAmountEl).text('$0.00');
+    $(totalPerPersonAmountEl).text('$0.00');
+    splitter.billAmount = 0;
+    splitter.tipPercentage = 0;
+    splitter.numberOfPeople = 0;
+    $('input').val('');
+}
+
+function toggleTheme() {
+    $('.theme-buttons button').on('click', e => {
+        let currentTheme = $('body').data('theme');
+        let id = $(e.target).closest('button').attr('id')
+        if(currentTheme === 'light-theme' && id === 'dark-theme') {
+            $('body').attr('data-theme', 'dark-theme') 
+        } else {
+            $('body').attr('data-theme', 'light-theme') 
+        }
+    })
+}
+
+function app () {
+    $('input').on('input', e => {
+        const inputID = $(e.target).attr('id');
+        const inputValue = Number($(e.target).val()); 
+        setupValues(inputID, inputValue);
+
+        // run this when all the values needed are stored
+        getCalculations();
+        
+    });
+
+    $('.app-wrapper button').click(function (e) { 
+        const buttonClass = $(e.target).attr('class');
+        // reset vakues
+        if(buttonClass === 'btn-reset'){
+            resetValues();
+        }
+
+        if(buttonClass === 'btn-tip') {
+            // get the value stored in data-tip attribute
+            splitter.tipPercentage = Number($(e.target).data('tip') / 100);
+            getCalculations();
+        }
+        
+    });
+}
